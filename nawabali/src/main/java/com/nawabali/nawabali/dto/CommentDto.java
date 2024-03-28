c1e0f76a127fc91ffb0f136f1d9a6cee86bb63c7
minnieming <eunmilee9109.1@gmail.com>
2024-03-28 16:29:33 +0900

    Comment 기본 세팅

--- nawabali/src/main/java/com/nawabali/nawabali/dto/CommentDto.java
+++ nawabali/src/main/java/com/nawabali/nawabali/dto/CommentDto.java
@@ -1,3 +1,13 @@
 package com.nawabali.nawabali.dto;
 
-import lombok.AllArgsConstru
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Slf4j(topic = "CommentDto 로그")
+public class CommentDto {
+}
