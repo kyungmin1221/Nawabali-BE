cc9ba936dce02a2c7bbab1592b00f6b4253e9a46
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-03-28 13:18:27 +0900

    초기 세팅

---
+++ nawabali/src/main/java/com/nawabali/nawabali/NawabaliApplication.java
@@ -0,0 +1,13 @@
+package com.nawabali.nawabali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NawabaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NawabaliApplication.class, args);
+	}
+
+}
---
+++ nawabali/src/test/java/com/nawabali/nawabali/NawabaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawabali.nawabali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NawabaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
