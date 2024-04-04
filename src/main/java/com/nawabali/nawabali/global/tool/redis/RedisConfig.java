5d37522002135bab7ca4c4e8d003fd7a9c4b90fe
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-04-04 14:53:42 +0900

    FIX : redis 호스트 및 비밀번호, EC2 기준으로 수정

--- src/main/java/com/nawabali/nawabali/global/tool/redis/RedisConfig.java
+++ src/main/java/com/nawabali/nawabali/global/tool/redis/RedisConfig.java
@@ -2,10 +2,12 @@ package com.nawabali.nawabali.global.tool.redis;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -17,10 +19,19 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     private final RedisProperties redisProperties;
 
-    // RedisProperties로 yaml에 저장한 host, post를 연결
+    // application.properties에 저장된 정보 호출
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    // RedisProperties에 저장한 port를 연결
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, redisProperties.getPort());
+        config.setPassword(password);
+        return new LettuceConnectionFactory(config);
     }
 
     // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
