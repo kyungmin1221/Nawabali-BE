38d833aef2397576614b163e4c3052fe0ff6603b
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-04-19 13:31:42 +0900

    fix : 불필요한 코드 삭제를 통한 디버깅

--- src/main/java/com/nawabali/nawabali/NawabaliApplication.java
+++ src/main/java/com/nawabali/nawabali/NawabaliApplication.java
@@ -4,14 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @OpenAPIDefinition(servers = {@Server(url = "https://hhboard.shop", description = "백엔드 서버")})
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableWebMvc
 public class NawabaliApplication {
 
 	public static void main(String[] args) {
