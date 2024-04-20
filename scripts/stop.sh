a9b5b331b9f83492a5b901f2abed169ad418670a
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-04-20 23:33:14 +0900

    fix : 오류 원인 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/build/libs/nawabali-0.0.1-SNAPSHOT.jar"
 
-IDLE_PORT=$(find_idle_port)
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
 TIME_NOW=$(date +%c)
-DEPLOY_LOG="/home/ubuntu/app/step3/deploy.log"
 
-echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
 
-if [ -z ${IDLE_PID} ]
-then
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "$TIME_NOW > 실행중인 $IDLE_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  echo "> kill -15 $IDLE_PID"
-  kill -15 ${IDLE_PID}
-  sleep 5
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
 fi
