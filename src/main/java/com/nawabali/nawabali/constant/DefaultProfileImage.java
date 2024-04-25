01f8bf13635b314e8000567a85c209b80be8d8a0
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-04-25 15:06:19 +0900

    feat : default 프로필 이미지 수정

--- src/main/java/com/nawabali/nawabali/constant/DefaultProfileImage.java
+++ src/main/java/com/nawabali/nawabali/constant/DefaultProfileImage.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class DefaultProfileImage {
-    public static String fileName = "default_profile_image.svg";
-    public static String imgUrl = "https://nawabalibucket.s3.ap-northeast-2.amazonaws.com/profileImage/ee0be4b1-9ee6-4358-83e8-eb34821ea93b.svg";
+    public static String fileName = "동네방네 프로필.svg";
+    public static String imgUrl = "https://nawabalibucket.s3.ap-northeast-2.amazonaws.com/profileImage/fa61c406-4d66-490e-a978-b21db918d3fc.svg";
 }
