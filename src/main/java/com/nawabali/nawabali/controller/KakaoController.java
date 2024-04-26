c4afce91fc895e8632b975eb595759fae6797f41
juwum12 <juwum12@naver.com>
2024-04-26 22:12:53 +0900

    FIX : redirect URL수정

--- src/main/java/com/nawabali/nawabali/controller/KakaoController.java
+++ src/main/java/com/nawabali/nawabali/controller/KakaoController.java
@@ -25,8 +25,8 @@ public class KakaoController {
             throws JsonProcessingException, IOException {
 
        String accessToken = kakaoService.kakaoLogin(code, response);
-//       return "redirect:https://www.dongnaebangnae.com/?accessToken=" + accessToken;
-       return "redirect:https://www.dongnaebangnae.com";
+       return "redirect:https://www.dongnaebangnae.com/?accessToken=" + accessToken;
+//       return "redirect:https://www.dongnaebangnae.com";
 
     }
 }
