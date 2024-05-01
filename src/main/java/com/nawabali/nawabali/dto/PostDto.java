8a38ae2e8fa02bf17a14a62722aa57eac3b6a91b
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-05-01 19:28:52 +0900

    Revert "Merge pull request #153 from Nawabali-project/develop"
    This reverts commit 7dbdee335073e06702ee042ce0e616580e7e3847, reversing
    changes made to e923cc11cb1e506e2a24ede7c447765541dcfb6e.

--- src/main/java/com/nawabali/nawabali/dto/PostDto.java
+++ src/main/java/com/nawabali/nawabali/dto/PostDto.java
@@ -79,8 +79,6 @@ public class PostDto {
 
         private String mainImageUrl;
 
-        private String resizedImageUrl;
-
         private boolean multiImages;
 
         private Long likesCount;
@@ -107,8 +105,7 @@ public class PostDto {
             this.longitude = post.getTown().getLongitude();
             this.createdAt = post.getCreatedAt();
             this.modifiedAt = post.getModifiedAt();
-            this.mainImageUrl = post.getImages().get(1).getImgUrl();
-            this.resizedImageUrl = post.getImages().get(0).getImgUrl();
+            this.mainImageUrl = post.getImages().get(0).getImgUrl();
             this.multiImages = post.getImages().size() > 1;
             this.commentCount = post.getComments().size();
         }
@@ -168,8 +165,7 @@ public class PostDto {
             this.createdAt = post.getCreatedAt();
             this.modifiedAt = post.getModifiedAt();
             this.imageUrls = post.getImages().stream()
-                    .map(PostImage::getImgUrl) // 리사이즈된 이미지 URL 제외
-                    .filter(imgUrl -> !imgUrl.contains("/compressed_postImages/"))
+                    .map(PostImage::getImgUrl)
                     .collect(Collectors.toList());
             this.commentCount = post.getComments().size();
             this.likesCount = likesCount;
