c036e264d917ac4e9690c8c177d5d052df68b1ec
minnieming <eunmilee9109.1@gmail.com>
2024-05-02 10:03:52 +0900

    FIX : subscribe할때 unreadMessageCount 보내기

--- src/main/java/com/nawabali/nawabali/service/ChatMessageService.java
+++ src/main/java/com/nawabali/nawabali/service/ChatMessageService.java
@@ -56,6 +56,7 @@ public class ChatMessageService {
                 }
             }
             notificationService.deleteAllNotification(userOptional,chatRoomId);
+            notificationService.notifyAllMyMessage(userOptional.getNickname());
             return;
         }
 
